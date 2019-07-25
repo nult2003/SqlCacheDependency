e7feebc5f90b18ef5eaa24814f17ed3f71738ab3
Minh Nhat LE <minh-nhat.le@bollore.com>
2019-07-25 14:22:39 +0700

    add project

---
+++ sqlCacheDependencyDemo/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace sqlCacheDependencyDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string ConnectionString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        // GET: Home
+        public ActionResult Index()
+        {
+            LoadMessages();
+            return View();
+        }
+
+        public ActionResult CheckSqlCache()
+        {
+            DataTable dtMessages = (DataTable)HttpContext.Cache.Get("Messages");
+            return RedirectToAction("Index");
+            //return Json("Ok", JsonRequestBehavior.AllowGet);
+        }
+
+        private DataTable LoadMessages()
+        {
+
+            DataTable dtMessages = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                SqlCommand command = new SqlCommand("Select [Code],[Name] from dbo.DropDown", connection);
+
+                //SqlCacheDependency dependency = new SqlCacheDependency(command);
+                SqlDependency dependency = new SqlDependency(command);
+                dependency.OnChange += this.dependency_OnChange;
+                
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+
+                dtMessages.Load(command.ExecuteReader(CommandBehavior.CloseConnection));
+                HttpContext.Cache.Insert("Messages", dtMessages);
+                //Cache.Insert("Messages", dtMessages, dependency);
+            }
+
+            return dtMessages;
+
+        }
+
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            SqlDependency dependency = sender as SqlDependency;
+            dependency.OnChange -= new OnChangeEventHandler(dependency_OnChange);
+            
+        }
+    }
+}
---
+++ sqlCacheDependencyDemo/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace sqlCacheDependencyDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            System.Data.SqlClient.SqlDependency.Start(connectionString);
+        }
+
+        protected void Application_End()
+        {
+            System.Data.SqlClient.SqlDependency.Stop(connectionString);
+        }
+    }
+}
